3042a1765ae1cbfe14cca201773d336cfbace9df
mimi99 <0mini9939@gmail.com>
2023-09-20 21:25:24 +0900

    github deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  basePath: "/",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
