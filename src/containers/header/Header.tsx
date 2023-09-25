e0fd61a1d5e8668c9760209cd9095cd696612821
mimi99 <0mini9939@gmail.com>
2023-09-25 23:25:58 +0900

    라우터 path 설정하기

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -2,28 +2,31 @@
 import React, { useState } from "react";
 import "./Header.scss";
 
-const navItems = {
+const pathList = {
   home: "",
   about: "about",
   post: "post",
   pratice: "pratice",
 } as const;
 
-type navItemsKeyType = keyof typeof navItems;
-type navItemsValueType = (typeof navItems)[navItemsKeyType];
+type pathKeyType = keyof typeof pathList;
+type pathValueType = (typeof pathList)[pathKeyType];
 
 const Header = () => {
   return (
     <header className={"header"}>
       <nav className={"navContainer"}>
         <ul className={"navItemList"}>
-          {Object.keys(navItems).map((item) => (
-            <li key={item}>
-              <a className={"linkItem"} href={"/" + item}>
-                {item}
-              </a>
-            </li>
-          ))}
+          {Object.keys(pathList).map((pathKey) => {
+            const path = pathKey as pathKeyType;
+            return (
+              <li key={path}>
+                <a className={"linkItem"} href={"/" + pathList[path]}>
+                  {pathKey}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
