41916cd26e09044188ff14d41825b935d13a75a9
mimi99 <0mini9939@gmail.com>
2023-09-27 01:06:22 +0900

    sass 디렉토리 구조 설정, header css 작업

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -14,13 +14,13 @@ type pathValueType = (typeof pathList)[pathKeyType];
 
 const Header = () => {
   return (
-    <header className={"header"}>
-      <nav className={"navContainer"}>
-        <ul className={"navItemList"}>
+    <header className="header">
+      <nav className="navContainer">
+        <ul className="navItemList">
           {Object.keys(pathList).map((pathKey) => {
             const path = pathKey as pathKeyType;
             return (
-              <li key={path}>
+              <li key={path} className="navItem">
                 <a className={"linkItem"} href={"/" + pathList[path]}>
                   {pathKey}
                 </a>
