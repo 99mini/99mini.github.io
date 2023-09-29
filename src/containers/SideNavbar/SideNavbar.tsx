a26deeb67e806f970fe6a1f691f331168b846dd0
mimi99 <0mini9939@gmail.com>
2023-09-29 12:23:26 +0900

    사이드바 위로 가기, 아래로 가기 버튼 구현

--- src/containers/SideNavbar/SideNavbar.tsx
+++ src/containers/SideNavbar/SideNavbar.tsx
@@ -1,10 +1,55 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import "./SideNavbar.scss";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const SideNavbar = () => {
+  const [isTopDisable, setIsTopDisable] = useState(false);
+
+  const handleScrollPostion = (destination: number) => {
+    window.scrollTo({ top: destination });
+  };
+
+  const handleScroll = () => {
+    const { scrollY } = window;
+    setIsTopDisable(scrollY < 200);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <aside className="asideNavbar">
-      <div className="contentWrapper">SideNavbar</div>
+      <div className="contentWrapper">
+        <ul className="buttonList">
+          <li className="buttonItem">
+            <button
+              className="moveToButton"
+              disabled={isTopDisable}
+              onClick={() => handleScrollPostion(0)}
+            >
+              <LastPageIcon style={{ transform: "rotate(-90deg)" }} />
+            </button>
+          </li>
+          <li className="buttonItem">
+            <button
+              className="moveToButton"
+              onClick={() =>
+                handleScrollPostion(document.documentElement.offsetHeight)
+              }
+            >
+              <ExpandMoreIcon />
+            </button>
+          </li>
+        </ul>
+      </div>
     </aside>
   );
 };
