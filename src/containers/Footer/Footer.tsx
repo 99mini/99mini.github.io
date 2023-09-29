495cbb6a4819356f98ad529656c0ea1b91fb25b4
mimi99 <0mini9939@gmail.com>
2023-09-29 12:56:43 +0900

    footer 레이아웃

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -1,7 +1,42 @@
 import "./Footer.scss";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+const linkIconList = {
+  github: <GitHubIcon key={"github-icon"} />,
+  insta: <InstagramIcon key={"insta-icon"} />,
+};
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="footer">
+      <ul className="footerItemList">
+        <ul className="iconList">
+          <li>
+            <a
+              href="https://github.com/99mini"
+              target="_blank"
+              aria-label="github"
+            >
+              {linkIconList.github}
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/youngmmiinn"
+              target="_blank"
+              aria-label="instagram"
+            >
+              {linkIconList.insta}
+            </a>
+          </li>
+        </ul>
+        <li>
+          <span>© 2023 99mini. All Rights Reserved.</span>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
