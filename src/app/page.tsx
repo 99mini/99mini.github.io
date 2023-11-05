b7ca93cf3b1fd987e93ee68a99ffb651f9a2e181
mimi99 <0mini9939@gmail.com>
2023-11-05 14:38:31 +0900

    feat: 페이드인 등장 애니메이션

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import { SideNavbar, MainSection, Home } from "../containers";
 
 const HomePage = () => {
   return (
-    <div>
+    <>
       <MainSection>
         <Home />
       </MainSection>
       <SideNavbar />
-    </div>
+    </>
   );
 };
 
